bec10ad83531097848a438b3760ea77afb8aa836
Michel-2004 <117313194+Michel-2004@users.noreply.github.com>
2023-06-19 11:32:08 +0200

    Add files via upload

---
+++ primer y segundo trimestre/5.3/Ejercicio2.java
@@ -0,0 +1,74 @@
+public class Ejercicio2 {
+    public static void main(String[] args){
+        int entero1 = 5;
+        int entero2 = 7;
+        //Ejercio 2.A
+        if ((entero1 > 5) && (entero2+3 < 10)) {
+            System.out.print("Ejercio 2.a");
+            System.out.println();
+        } else {
+            System.out.print("Ejercio 2.a else");
+            System.out.println();
+        }
+
+        //Ejercicio 2.B
+        if ((10 !=entero1+5) || (20 > (entero2 + 5)* entero1+5)) {
+            System.out.print("Ejercicio2.b");
+            System.out.println();
+        } else {
+            System.out.print("Ejercicio2.b else");
+            System.out.println();
+        }
+
+        //Ejercicio 2.C
+        System.out.print("Ejercicio 2.c: " + (entero1* entero2 + 10 + 4 / 2 - 2 * 4 - 2));
+        System.out.println();
+        
+        //Ejercicio 2.D
+        if ((10 / 2-1 ==entero1) && (20 >entero2 + 13)) {
+            System.out.print("Ejercio 2.d");
+            System.out.println();
+        } else {
+            System.out.print("Ejercio 2.d else");
+            System.out.println();
+        }
+
+        //Ejercicio 2.E
+        if ((5 - 2 > 4) != (5 == 25/5)) {
+            System.out.print("Ejercio 2.e");
+            System.out.println();
+        } else {
+            System.out.print("Ejercio 2.e else");
+            System.out.println();
+        }
+
+        //Ejercicio 2.F
+         if (("J" == "J") || ("S" == "s")) {
+            System.out.print("Ejercicio 2.f");
+            System.out.println();
+        } else {
+            System.out.print("Ejercicio 2.f else");
+            System.out.println();
+        }
+
+        //Ejercicio 2.G
+        if ((4*2 <= 8) || (2*2 < 5) && (4 > 3 + 1)) {
+            System.out.print("Ejercicio 2.g");
+            System.out.println();
+        } else {
+            System.out.print("Ejercicio 2.g else");
+            System.out.println();
+        }
+
+        //Ejercicio 2.H
+        if ((10 + 4 < 15 - 3) || (2 * 5  + 1 > 14 - 2 *2 )) {
+            System.out.print("Ejercicio 2.h");
+            System.out.println();
+        } else {
+            System.out.print("Ejercicio 2.h else");
+            System.out.println();
+        }
+
+    }
+
+}
